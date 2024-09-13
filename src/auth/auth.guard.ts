f3edbe9437f417123111180f00a4e7fdcc2ff91a
Giovanny Prens Luna <69285209+GPrensLuna@users.noreply.github.com>
2024-09-13 11:01:41 -0500

    prueba de auth

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -13,10 +13,10 @@ export class AuthGuard implements CanActivate {
   public async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<AuthRequest>();
 
-    const authHeader = request.headers["authentication"] as string | undefined;
+    const authHeader = request.headers["Authentication"] as string | undefined;
 
     if (!authHeader) {
-      this.logger.warn("Authorization header missing");
+      this.logger.warn(`Authorization header missing ${authHeader}}`);
       throw new UnauthorizedException("Token de autenticación no proporcionado.");
     }
 
