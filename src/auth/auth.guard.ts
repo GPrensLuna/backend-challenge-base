7958346caa26612d7adca380a10ec7570c328047
Giovanny Prens Luna <69285209+GPrensLuna@users.noreply.github.com>
2024-09-13 11:18:11 -0500

    prueba de auth

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-base-to-string */
 /* eslint-disable no-console */
-import { Injectable, UnauthorizedException, Logger } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import type { CanActivate, ExecutionContext } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import type { AuthRequest } from "./interfaces/AuthRequest";
@@ -8,35 +8,26 @@ import type { UserDataDto } from "./dto";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  private readonly logger = new Logger(AuthGuard.name);
-
   public constructor(private readonly jwtService: JwtService) {}
 
   public async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<AuthRequest>();
-    console.log(`request ${request}`);
+    console.log(request);
+    const authHeader = request.headers["authentication"] as string | undefined;
+    console.log("authHeader", authHeader);
 
-    const authHeader = request.headers["Authentication"] as string | undefined;
-    console.log(`authHeader ${authHeader}`);
-    if (!authHeader) {
-      console.log(`authHeader ${authHeader}`);
-
-      throw new UnauthorizedException("Token de autenticación no proporcionado.");
-    }
-
-    if (!authHeader.startsWith("Bearer ")) {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
       throw new UnauthorizedException("Token de autenticación no proporcionado.");
     }
 
     const token = authHeader.split(" ")[1];
+    console.log("authHeader", token);
 
     try {
       const decodedToken = await this.jwtService.verifyAsync(token);
       request.user = decodedToken as UserDataDto;
       return true;
-    } catch (error) {
-      console.log(`error ${error}`);
-
+    } catch {
       throw new UnauthorizedException("Token de autenticación inválido.");
     }
   }
